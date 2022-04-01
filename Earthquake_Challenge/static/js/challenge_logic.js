b23a36d5a0812d4422c3883962a7e9dbe779f727
Sara <95581411+saraegregg@users.noreply.github.com>
2022-04-01 14:36:18 -0500

    Adding Deliverable 3.

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -15,6 +15,13 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 	accessToken: API_KEY
 });
 
+//Create third tile layer to be a background option
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
 	center: [40.7, -94.5],
@@ -24,8 +31,9 @@ let map = L.map('mapid', {
 
 // Create a base layer that holds all three maps.
 let baseMaps = {
-  "Streets": streets,
-  "Satellite": satelliteStreets
+  "Street View": streets,
+  "Satellite View": satelliteStreets,
+  "Dark View": dark
 };
 
 // 1. Add a layer groups for the tectonic plate and major earthquake data.
@@ -129,10 +137,10 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
   // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
   function getColor(magnitude) {
     if (magnitude > 6) {
-      return "#000000";
+      return "#ca2cea";
     }
     if (magnitude >= 5) {
-      return "#800000";
+      return "#bf249b";
     }
     if (magnitude < 5) {
       return "#ea2c2c";
@@ -203,7 +211,7 @@ legend.onAdd = function() {
 
   // Add style for the tectonic plates
   let tecStyle = {
-    color: "darkmagenta",
+    color: "dodgerblue",
     weight: 3
   };
 
